93fc65b416bfc014dd42b60c691a83c59b8b655f
Shivam Sharma <shivam0991sharma@gmail.com>
2026-09-16 00:27:11 +0530

    adjacent list method is apply

--- graphs/day_1.cpp
+++ graphs/day_1.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 using namespace std;
 
+void adj_list();
+
 void matrix_method();
 int main(){
-    matrix_method();
-
+    //matrix_method();
+    adj_list();
     return 0;
 }
 
@@ -25,4 +27,26 @@ void matrix_method(){
         }
         cout<<endl;
     }
+}
+
+
+void adj_list(){
+    int n,m;
+    cin>>n>>m;
+    vector<int>arr[n+1];
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }
+
+    for(int i=1;i<=n;i++){
+        cout<<i<<"->";
+        for(int j=0;j<arr[i].size();j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
 }
