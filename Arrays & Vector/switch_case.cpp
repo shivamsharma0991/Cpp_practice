82991eee358455ce923f25f81fe94bbed87479fe
Shivam Sharma <shivam0991sharma@gmail.com>
2026-09-22 10:38:05 +0530

    switch case and vectors in cpp

---
+++ Arrays & Vector/switch_case.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    int day;
+    cout<<"enter the no of day: ";
+    cin>>day;
+
+    switch(day){
+        case 1:
+            cout<<"Monday";
+            break;
+        case 2:
+            cout<<"Tuesday";
+            break;
+        case 3:
+            cout<<"Wednesday";
+            break;
+        case 4:
+            cout<<"Thrusday";
+            break;
+        case 5:
+            cout<<"Friday";
+            break;
+        case 6:
+            cout<<"Saturday";
+            break;
+        case 7:
+            cout<<"Sunday";
+            break;
+        default:
+            cout<<"not in the limit. "
+        
+    }
+    return 0;
+}
---
+++ Arrays & Vector/vectors.cpp
@@ -0,0 +1,31 @@
+#include<bits//stdc++.h>
+using namespace std;
+
+int main(){
+
+    //syntax
+    vector<int>a;
+    a.push_back(1);
+    a.emplace_back(2);
+
+    //pairs in vecter
+    vector<pair<int,int>>b;
+
+    b.puch_back({1,2});
+    b.emplace_back(3.4);
+
+    //vector with already defined size
+
+    vector<int>c(5,100);
+    vector<int>d(5);
+
+    //copy
+    vector<int>v1(5,20);
+    vector<int>v2(v1);
+
+    //access vector elements
+
+    cout<<v1[0]
+    
+
+}
